c00a786b6ad4af06a3410eddbd8883779f266c69
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2017-04-05 13:31:06 -0400

    now accepts POST requests

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,5 +1,5 @@
 # Comma-separated list of ArchivesSpace API endpoints you want accessible via Javascript
-CORS_ENDPOINTS = ['/version', '/users/current-user', '/repositories/:repo_id/find_by_id/archival_objects', '/repositories/:repo_id/archival_objects/:id', '/repositories/:repo_id/resources/:id']
+CORS_ENDPOINTS = ['/version', '/users/current-user', '/repositories/:repo_id/find_by_id/archival_objects', '/repositories/:repo_id/archival_objects/:id', '/repositories/:repo_id/resources/:id', '/repositories/:repo_id/search', '/locations/:id', '/repositories/:repo_id/resources/:id/tree', '/repositories/:repo_id/top_containers']
 
 class CORSMiddleware
 
@@ -18,9 +18,21 @@ class CORSMiddleware
 
       # Add CORS headers
       headers = result[1]
-      headers["Access-Control-Allow-Origin"] = "*"
-      headers["Access-Control-Allow-Methods"] = "GET"
-      headers["Access-Control-Allow-Headers"] = "X-ArchivesSpace-Session"
+      headers["Access-Control-Allow-Origin"] =  "*" # This should be changed
+      headers["Access-Control-Allow-Methods"] = "GET, POST"
+      headers["Access-Control-Allow-Headers"] = "X-ArchivesSpace-Session, Content-Type"
+    end
+	
+	 # Add CORS headers to specific endpoints
+    if env['REQUEST_METHOD'] == 'POST' &&
+       result[0] == 200 &&
+       @patterns.any? {|pattern| env['PATH_INFO'] =~ pattern}
+
+      # Add CORS headers
+      headers = result[1]
+      headers["Access-Control-Allow-Origin"] =  "*" # This should be changed
+      headers["Access-Control-Allow-Methods"] = "GET, POST"
+      headers["Access-Control-Allow-Headers"] = "X-ArchivesSpace-Session, Content-Type"
     end
 
     result
@@ -40,9 +52,9 @@ end
 # Support OPTIONS, which is necessary for certain browsers (for example Google Chrome)
 CORS_ENDPOINTS.each do |uri|
   ArchivesSpaceService.options uri do
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET"
-    response.headers["Access-Control-Allow-Headers"] = "X-ArchivesSpace-Session"
+    response.headers["Access-Control-Allow-Origin"] = "*"  # This should be changed
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST"
+    response.headers["Access-Control-Allow-Headers"] = "X-ArchivesSpace-Session,  Content-Type"
 
     halt 200
   end
